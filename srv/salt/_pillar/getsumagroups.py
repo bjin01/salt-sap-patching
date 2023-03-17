167c06feaca290968b360cc38279aabfbe6d0fc5
bjin01 <bo.jin@suse.com>
2023-03-17 20:58:14 +0100

    getsumagroup.py added

---
+++ srv/salt/_pillar/getsumagroups.py
@@ -0,0 +1,204 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+# Import python libs
+from cryptography.fernet import Fernet
+import atexit
+import os
+import logging
+import yaml
+# Import third party libs
+from salt.ext import six
+from datetime import datetime,  timedelta
+
+from typing import Any, TYPE_CHECKING
+if TYPE_CHECKING:
+    __salt__: Any = None
+    __opts__: Any = None
+    
+__virtualname__ = "getsumagroups"
+log = logging.getLogger(__name__)
+
+_sessions = {}
+
+
+def __virtual__():
+    '''
+    Check for spacewalk configuration in master config file
+    or directory and load runner only if it is specified
+    '''
+    if not _suma_configuration():
+        return False, 'No suma_api configuration found'
+    return True
+
+def _suma_configuration(suma_url=''):
+    '''
+    Verify if suma_api configuration is set otherwise not loading this module.
+    '''
+    if 'suma_api' in __opts__:    
+        return True
+    else:
+        return False
+    
+
+def _decrypt_password(password_encrypted):
+    print("-------------os env sumakey {}".format(os.environ))
+    encrypted_pwd = ""
+    if not os.path.exists("/srv/pillar/sumakey.sls"):
+        print("No /srv/pillar/sumakey.sls found")
+        if os.environ.get('SUMAKEY') == None: 
+            log.fatal("You don't have ENV SUMAKEY set. Use unencrypted pwd.")
+            return str(password_encrypted)
+        else:
+            print("----------------os env sumakey found.")
+            encrypted_pwd = os.environ.get('SUMAKEY')
+    else:
+        print("--------sumakey.sls found.")
+        with open("/srv/pillar/sumakey.sls", 'r') as file:
+            # Load the YAML data into a dictionary
+            sumakey_dict = yaml.safe_load(file)
+            encrypted_pwd = sumakey_dict["SUMAKEY"]
+
+    if not encrypted_pwd == "":
+        saltkey = bytes(str(encrypted_pwd), encoding='utf-8')
+        fernet = Fernet(saltkey)
+        encmessage = bytes(str(password_encrypted), encoding='utf-8')
+        pwd = fernet.decrypt(encmessage)
+    else:
+        log.fatal("encrypted_pwd is empty. Use unencrypted pwd.")
+        return str(password_encrypted)        
+    
+    return pwd.decode()
+
+def _get_suma_configuration(suma_url=''):
+    '''
+    Return the configuration read from the master configuration
+    file or directory
+    '''
+    suma_config = __opts__['suma_api'] if 'suma_api' in __opts__ else None
+
+    if suma_config:
+        try:
+            for suma_server, service_config in six.iteritems(suma_config):
+                username = service_config.get('username', None)
+                password_encrypted = service_config.get('password', None)
+                password = _decrypt_password(password_encrypted)
+                protocol = service_config.get('protocol', 'https')
+
+                if not username or not password:
+                    log.error(
+                        'Username or Password has not been specified in the master '
+                        'configuration for %s', suma_server
+                    )
+                    return False
+
+                ret = {
+                    'api_url': '{0}://{1}/rpc/api'.format(protocol, suma_server),
+                    'username': username,
+                    'password': password,
+                    'servername': suma_server
+                }
+
+                if (not suma_url) or (suma_url == suma_server):
+                    return ret
+        except Exception as exc:  # pylint: disable=broad-except
+            log.error('Exception encountered: %s', exc)
+            return False
+
+        if suma_url:
+            log.error(
+                'Configuration for %s has not been specified in the master '
+                'configuration', suma_url
+            )
+            return False
+
+    return False
+
+def _get_client_and_key(url, user, password, verbose=0):
+    '''
+    Return the client object and session key for the client
+    '''
+    session = {}
+    session['client'] = six.moves.xmlrpc_client.Server(url, verbose=verbose, use_datetime=True)
+    session['key'] = session['client'].auth.login(user, password)
+
+    return session
+
+
+def _disconnect_session(session):
+    '''
+    Disconnect API connection
+    '''
+    session['client'].auth.logout(session['key'])
+
+
+def _get_session(server):
+    '''
+    Get session and key
+    '''
+    if server in _sessions:
+        return _sessions[server]
+
+    config = _get_suma_configuration(server)
+    if not config:
+        raise Exception('No config for \'{0}\' found on master'.format(server))
+
+    session = _get_client_and_key(config['api_url'], config['username'], config['password'])
+    atexit.register(_disconnect_session, session)
+
+    client = session['client']
+    key = session['key']
+    _sessions[server] = (client, key)
+
+    return client, key
+
+
+def ext_pillar(minion_id, pillar, *args, **kwargs):
+
+    my_pillar = {"external_pillar": {}}
+    my_pillar["external_pillar"]['sumagroups'] = get_groups(minion_id)
+    log.info("-----------{}-----{}--------------".format(minion_id, my_pillar))
+    return my_pillar
+
+
+def get_groups(minion_id, **kwargs):
+    groups = dict()
+    suma_config = _get_suma_configuration()
+    server = suma_config["servername"]
+    print("server var: {}".format(server))
+    if not suma_config:
+        raise Exception('No config for \'{0}\' found on master'.format(server))
+
+    try:
+        client, key = _get_session(suma_config['servername'])
+    except Exception as exc:  # pylint: disable=broad-except
+        err_msg = 'Exception raised when connecting to suse manager server ({0}): {1}'.format(server, exc)
+        log.error(err_msg)
+        
+        return {'Error': err_msg}
+    try:
+        systemid = client.system.getId(key, minion_id)
+    except Exception as exc:  # pylint: disable=broad-except
+        err_msg = 'Exception raised when trying to get system ID ({0}): {1}'.format(server, exc)
+        log.error(err_msg)
+        
+        return {'Error': err_msg}
+
+    try:
+        grouplist = client.system.listGroups(key, systemid[0]['id'])
+    except Exception as exc:  # pylint: disable=broad-except
+        err_msg = 'Exception raised when trying to get system group list ({0}): {1}'.format(server, exc)
+        log.error(err_msg)
+        
+        return {'Error': err_msg}
+    
+    if len(grouplist) != 0:
+        for i in grouplist:
+            if i['subscribed'] == 1:
+                groups[i['system_group_name']] = minion_id
+    
+
+    return groups
+    
+if __name__ == '__main__':
+   output = get_groups("vmsumaprx804p01.svz.admin.ch")
+   print(output)
