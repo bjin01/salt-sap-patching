b2ec6eb313b6f0ec6e0758d23013105541a9a6b3
bjin01 <bo.jin@suse.com>
2023-06-08 15:52:47 +0200

    updated check_zypper_refresh.sh

--- srv/salt/orch/zypper/check_zypper_refresh.sh
+++ srv/salt/orch/zypper/check_zypper_refresh.sh
@@ -18,7 +18,14 @@ endTime=$(( $(date +%s) + $TIMEOUT ))
 
 if [ "$ZYPPER" = true ]
 then
-    output=$(timeout -v -s SIGKILL $TIMEOUT zypper -q refresh 2>&1 >/dev/null)
+    output=$(timeout --preserve-status -s SIGKILL $TIMEOUT zypper -q refresh 2>&1 >/dev/null)
+    if [ $? -eq 137 ]
+    then
+        echo "It is timeout and killed"
+	COMMENT="ERROR: timeout and zypper killed"
+        echo "changed=yes comment='${COMMENT}'"
+	exit 137
+    fi
     if [ -z "$output" ] && [ $? = 0 ]
     then
 	COMMENT="All is good. zypper ref successful."
